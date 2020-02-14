2248870f4aa0d98ef7234547b4143eb2c5274723
Carl Olson <43393940+Hunter71a@users.noreply.github.com>
2020-02-13 19:30:11 -0500

    Add game over screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, StyleSheet, Text, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
-
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -17,22 +16,57 @@ const generateRandomBetween = (min, max, exclude) => {
 };
 
 const GameScreen = props => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice)
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomBetween(1, 100, props.userChoice)
     );
+    const [rounds, setRounds] = useState(0);
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const {userChoice, onGameOver} = props;
+
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
 
     const nextGuessHandler = direction => {
-        if ((direction === 'lower' && currentGuess < props.userChoice)         
-        || (direction === 'higher' && currentGuess > props.userChoice))
-        Alert.alert('Are you sure that is correct?')
+        if (
+            (direction === 'lower' && currentGuess < props.userChoice) ||
+            (direction === 'higher' && currentGuess > props.userChoice)
+        ) {
+            Alert.alert('Are you serious?',
+                'I can only imagine you sitting there slackjawed wondering what number you picked as a spittle of drool runs out of the corner of your open mouth.', [
+                { text: 'Soory!', style: 'cancel' }
+            ]);
+            return;
+        }
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess;
+
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1);
     };
+    // /*      
+    //       const randomAlert = Math.floor(Math.random() * 3) + 1;
+    //             if (randomAlert === 1) {
+    //                 Alert.alert('Error alert!',  message='Are you having trouble remembering the number you chose?');}
+    //         Alert.alert('Wrong button', message='Are you sure that is correct button to press?');
+
+    //     };*/
 
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess: </Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower' )} />
-                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'higher')} />
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+                <Button title="HIGHER" onPress={nextGuessHandler.bind(this, 'higher')} />
             </Card>
         </View>
     );
@@ -47,7 +81,7 @@ const styles = StyleSheet.create({
         maxWidth: '80%'
     },
     screen: {
-        flex: 1, 
+        flex: 1,
         padding: 10,
         alignItems: 'center'
     }
